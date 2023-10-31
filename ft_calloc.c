f78d6f4a1cab6674eb8aa2d638eb53d02fad4a9e
NourMellal <mellal.nour9@gmail.com>
2023-10-31 16:56:17 +0100

    add

--- ft_calloc.c
+++ ft_calloc.c
@@ -5,22 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: nmellal <nmellal@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/30 20:15:36 by nmellal           #+#    #+#             */
-/*   Updated: 2023/10/30 20:18:13 by nmellal          ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
-#include "libft.h"
-
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_calloc.c                                        :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: fpetras <fpetras@student.42.fr>            +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/14 12:21:40 by fpetras           #+#    #+#             */
-/*   Updated: 2019/03/22 17:07:07 by fpetras          ###   ########.fr       */
+/*   Updated: 2023/10/31 16:44:36 by nmellal          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,11 +14,11 @@
 
 void	*ft_calloc(size_t count, size_t size)
 {
-    void    *res;
+	void	*res;
 
-    res = malloc(size * count);
-    if (!res)
-        reurn (NULL);
-    ft_bzero(res, size * count);
-    return (res);
+	res = malloc(size * count);
+	if (!res)
+		reurn(NULL);
+	ft_bzero(res, size * count);
+	return (res);
 }
--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: nmellal <nmellal@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 16:24:24 by nmellal           #+#    #+#             */
-/*   Updated: 2023/10/30 21:00:46 by nmellal          ###   ########.fr       */
+/*   Updated: 2023/10/31 16:48:39 by nmellal          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,21 +47,22 @@ char	*ft_strtrim(char const *s1, char const *set)
 	return (ft_strncpy(res, &s1[start], end - start + 1));
 }
 
-int main(int ac, char **av)
-{
-	if (ac == 3)
-	{
-		char *tmp = ft_strtrim(av[1], av[2]);
-		if (tmp)
-		{
-			for (int i = 0; tmp[i]; i++)
-			{
-				putchar(tmp[i]);
-			}
-			free(tmp);
-		}
-		else
-			printf("NULL");
-	}
-	printf("\n"); return (0);
-}
+// int	main(int ac, char **av)
+// {
+// 	if (ac == 3)
+// 	{
+// 		char *tmp = ft_strtrim(av[1], av[2]);
+// 		if (tmp)
+// 		{
+// 			for (int i = 0; tmp[i]; i++)
+// 			{
+// 				putchar(tmp[i]);
+// 			}
+// 			free(tmp);
+// 		}
+// 		else
+// 			printf("NULL");
+// 	}
+// 	printf("\n");
+// 	return (0);
+// }
